1c58772f33ca7c5f64dcef33f9b5a736e8454fe4
Uriel <82734@alumnos.utleon.edu.mx>
2026-02-21 09:31:17 -0600

    Migraciones 21/02/2026

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask import flash
 from flask_wtf.csrf import CSRFProtect
 from config import DevelopmentConfig
+from flask_migrate import Migrate
 from flask import g
 import forms
 
@@ -10,6 +11,8 @@ from models import Alumno
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
+db.init_app(app)
+migrate = Migrate(app, db)
 csrf = CSRFProtect()
 
 @app.errorhandler(404)
@@ -28,12 +31,60 @@ def alumnos():
 	create_form = forms.UserForm(request.form)
 	if request.method == 'POST':
 		alum = Alumno(nombre=create_form.nombre.data,
-					  apaterno=create_form.apaterno.data,
+					  apellidos=create_form.apellidos.data,
+					  telefono=create_form.telefono.data,
 					  email=create_form.email.data)
 		db.session.add(alum)
 		db.session.commit()
 		return redirect(url_for('index'))
 	return render_template("Alumnos.html", form=create_form)
+
+@app.route("/modificar", methods=['GET', 'POST'])
+def modificar():
+	create_form = forms.UserForm(request.form)
+	if request.method == 'GET':
+		id=request.args.get('id')
+		alm1 = db.session.query(Alumno).filter(Alumno.id==id).first()
+		id=request.args.get('id')
+		create_form.id.data=alm1.id
+		create_form.nombre.data=alm1.nombre
+		create_form.apellidos.data=alm1.apellidos
+		create_form.telefono.data=alm1.telefono
+		create_form.email.data=alm1.email
+		
+	if request.method == 'POST':
+		id=request.args.get('id')
+		alm1 = db.session.query(Alumno).filter(Alumno.id==id).first()
+		alm1.id=id
+		alm1.nombre=create_form.nombre.data
+		alm1.apellidos=create_form.apellidos.data
+		alm1.telefono=create_form.telefono.data
+		alm1.email=create_form.email.data
+		db.session.add(alm1)
+		db.session.commit()
+		return redirect(url_for('index'))
+	return render_template("modificar.html", form=create_form)
+
+@app.route("/eliminar", methods=['GET', 'POST'])
+def eliminar():
+	create_form = forms.UserForm(request.form)
+	if request.method == 'GET':
+		id=request.args.get('id')
+		alm1 = db.session.query(Alumno).filter(Alumno.id==id).first()
+		id=request.args.get('id')
+		create_form.id.data=alm1.id
+		create_form.nombre.data=alm1.nombre
+		create_form.apellidos.data=alm1.apellidos
+		create_form.telefono.data=alm1.telefono
+		create_form.email.data=alm1.email
+		
+	if request.method == 'POST':
+		id=create_form.id.data
+		alm=Alumno.query.get(id)
+		db.session.delete(alm)
+		db.session.commit()
+		return redirect(url_for('index'))
+	return render_template("eliminar.html", form=create_form)
 
 @app.route("/detalles", methods=['GET', 'POST'])
 def detalles():
@@ -43,14 +94,13 @@ def detalles():
 		alm1 = db.session.query(Alumno).filter(Alumno.id==id).first()
 		id=request.args.get('id')
 		nombre=alm1.nombre
-		apaterno=alm1.apaterno
+		apaterno=alm1.apellidos
 		email=alm1.email
 
 	return render_template('detalles.html', nombre=nombre, apaterno=apaterno, email=email)
 
 if __name__ == '__main__':
 	csrf.init_app(app)
-	db.init_app(app)
 
 	with app.app_context():
 		db.create_all()
--- forms.py
+++ forms.py
@@ -5,7 +5,8 @@ from wtforms import validators
 class UserForm(FlaskForm): 
     id = IntegerField('id', [validators.NumberRange(min=1, max=20, message='Valor no válido')])
     nombre = StringField('nombre', [validators.DataRequired(message='El nombre es obligatorio'),validators.Length(min=4, max=20, message='Requiere entre 4 y 20 caracteres')])
-    apaterno = StringField('apaterno', [validators.DataRequired(message='El apellido es requerido')])
+    apellidos = StringField('apellidos', [validators.DataRequired(message='El apellido es requerido')])
+    telefono = StringField('telefono', [validators.DataRequired(message='El teléfono es requerido')])
     email = EmailField('correo', [validators.DataRequired(message='El correo es requerido'),validators.Email(message='Ingresa un correo válido')])
     
     submit = SubmitField('Enviar')
